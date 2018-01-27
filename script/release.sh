050b6ec81288d87531d56d3d291def8dbf791c87
Taiji Miyabe <a1153tm@aiit.ac.jp>
2018-01-27 23:03:16 +0900

    S3アーティファクトチェックを追加

--- script/release.sh
+++ script/release.sh
@@ -2,8 +2,10 @@
 set -x
 set -e
 
-_repositories=`cat config/release.json`
-_repositories=`echo ${_repositories} | jq --compact-output ".repository"`
+_json=`cat config/release.json`
+
+# リポジトリ名を取得する
+_repositories=`echo ${_json} | jq --compact-output ".repository"`
 _repositories=`echo ${_repositories} \
     | sed -e "s/,/ /g" \
     | sed -e s/\"//g \
@@ -16,29 +18,41 @@ do
     fi
 done
 
-_branch=`cat config/release.json`
-_branch=`echo ${_branch} | jq ".branch"`
+# ブランチ名を取得する
+_branch=`echo ${_json} | jq ".branch"`
 _branch=`echo ${_branch} | sed -e s/\"//g`
 
 rm -fr /tmp/aaa
 mkdir /tmp/aaa
 cd /tmp/aaa
 
-_tag=${DRONE_COMMIT_BRANCH}
 _base_dir=`pwd`
+
+# リポジトリをチェックアウトする。そのあとS3にアーティファクトがあるか確認する
 for _repository in ${_repositories}
 do
   _repository_url=https://github.com/a1153tm/${_repository}.git
-  cd ${_base_dir}
-  git clone ${_repository_url}
-  cd ${_repository}
+  cd ${_base_dir} && git clone ${_repository_url} && cd ${_repository}
+  git checkout ${_branch}
+  _commit=`git log -1 | head -1 | cut -f2 -d " "`
+  _s3_url=s3://backet/${_repository}/${_commit}.jar
+  echo $_s3_url
+done
+
+# タグを打つ
+_tag=${DRONE_COMMIT_BRANCH}
+for _repository in ${_repositories}
+do
+  cd ${_base_dir}/${_repository}
+  # xxx_release_prevタグを削除する
   set +e
   git tag -d ${_tag}_prev
   if [ $? -eq 0 ]; then
     set -e
     git push origin :${_tag}_prev
   fi
-  set -e
+  # xxx_releaseタグをxxx_release_prevタグにリネームする
+  set +e
   git tag ${_tag}_prev ${_tag}
   if [ $? -eq 0 ]; then
     set -e
@@ -46,6 +60,7 @@ do
     git tag -d ${_tag}
     git push origin :${_tag}
   fi
+  # releaseブランチの最終コミットにxxx_releaseタグを打つ
   set -e
   git checkout ${_branch}
   git tag ${_tag}
